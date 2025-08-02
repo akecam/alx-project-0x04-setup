6415aabe51dca5ebdd4f55e4cea59602a562e328
Donald Umeh <donaldumeh54@gmail.com>
2025-08-02 02:11:58 +0100

    contextapi

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { CountProvider } from "@/context/CountContext";
+import { CountProvider } from "@/context/countContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
